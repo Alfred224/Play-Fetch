e7758f6d44eaa52c5c0bfde9b0070e11767a2e9d
Alfred224 <97020796+Alfred224@users.noreply.github.com>
2022-08-28 11:51:25 -0700

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    // This is the time interval required for the player to be able to spawn another dog
+    private float dogRespawnTime = 2.0f;
+
+    // The "preTime" has been initiallized in a way to allow the "dogRespawnTime" to take effect after spawning the dog for the first time
+    private float preTime = -2.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - preTime > dogRespawnTime)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            preTime = Time.time;
+        }
+    }
+}
